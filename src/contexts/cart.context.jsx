9022feaa572d92c0b345a31367e687935e6d7247
wendyyng <ng_yih_wei@hotmail.com>
2022-09-15 12:37:34 -0700

    Create CartItem component, addCartItem method in cart context and show updated cart items in cart dropdown

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,37 @@
 import { createContext, useState } from "react";
 
+export const addCartItem = (cartItems, productToAdd) => {
+    //find if cart items contains productToAdd
+    const existingCartItem = cartItems.find(
+        (cartItems) => cartItems.id === productToAdd.id
+    )
+    //if found, increment quantity
+    if(existingCartItem) {
+        return cartItems.map((cartItem) => cartItem.id === productToAdd.id 
+        ? {...cartItem, quantity: cartItem.quantity + 1}
+        : cartItem
+        )
+    }
+
+    //return new array with modified cart Items/new cart item
+    return [...cartItems, {...productToAdd, quantity: 1}]
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => {}
+    setIsCartOpen: () => {},
+    cartItems: [],
+    addItemToCart: () => {}
 })
 
 export const CartProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
-    const value = { isCartOpen, setIsCartOpen}
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd))
+    }
+    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems}
     return(
         <CartContext.Provider value={value}>{children}</CartContext.Provider>
     )
