5d601dad009710fbc1d2c4d44626b7b07959f2bb
wendyyng <ng_yih_wei@hotmail.com>
2022-09-14 23:22:29 -0700

    Add Product Cards and Styling

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,16 +1,16 @@
 import { useContext } from 'react';
 import { ProductsContext } from '../../contexts/products.context';
 import SHOP_DATA from '../../shop-data.json'
+import ProductCard from '../../components/product-card/product-card.component';
+import './shop.styles.scss'
 
 const Shop = () => {
-    const {products} = useContext(ProductsContext);
-    
+    const { products } = useContext(ProductsContext);
+
     return (
-        <div>
-            {SHOP_DATA.map(({id, name}) => (
-                <div key={id}>
-                    <h1>{name}</h1>
-                </div>
+        <div className="products-container">
+            {products.map((product) => (
+              <ProductCard key={product.id} product={product}/>
             )
             )}
         </div>
