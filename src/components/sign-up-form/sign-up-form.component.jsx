8828d425ec154743295996f3420f0c2b01cbfb53
wendyyng <ng_yih_wei@hotmail.com>
2022-09-14 17:45:34 -0700

    Show sign out when current user exists and update current user when user signs up

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import { useState, useContext  } from 'react'
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
 import FormInput from '../form-input/form-input.component'
 import './sign-up-form.styles.scss'
 import '../button/button.component'
 import Button from '../button/button.component'
+import { UserContext } from '../../contexts/user.context'
 
 const defaultFormFields = {
     displayName: '',
@@ -16,6 +17,10 @@ const SignUpFrom = () => {
     const [formFields, setFormFields] = useState(defaultFormFields)
     const { displayName, email, password, confirmPassword } = formFields;
 
+    //function getting rerun when user signs in because of userContext
+    console.log('hit');
+
+    const { setCurrentUser } = useContext(UserContext)
     console.log(formFields)
 
     const resetFormFields = () => {
@@ -35,6 +40,7 @@ const SignUpFrom = () => {
             const { user } = await createAuthUserWithEmailAndPassword(email, password)
             console.log(user)
 
+            setCurrentUser(user)
             await createUserDocumentFromAuth(user, { displayName });
             resetFormFields();
         } catch(error){
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -17,9 +17,12 @@ const Navigation = () => {
             <Link className="nav-link" to="/shop">
                 SHOP
             </Link>
-            <Link className="nav-link" to="/auth">
+            { currentUser ? (
+              <span className="nav-link">SIGN OUT</span>
+            ): (<Link className="nav-link" to="/auth">
                 SIGN IN
             </Link>
+            )}
           </div>
         </div>
         <Outlet />
