662d25189bfc76e560b3d043ea60225015d61bd5
wendyyng <ng_yih_wei@hotmail.com>
2022-09-14 23:09:13 -0700

    Create Products Context

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,7 +1,10 @@
-
+import { useContext } from 'react';
+import { ProductsContext } from '../../contexts/products.context';
 import SHOP_DATA from '../../shop-data.json'
 
 const Shop = () => {
+    const {products} = useContext(ProductsContext);
+    
     return (
         <div>
             {SHOP_DATA.map(({id, name}) => (
