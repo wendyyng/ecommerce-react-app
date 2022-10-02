dc18e590a306b462cc4c10a524720a96ed467ebb
wendyyng <ng_yih_wei@hotmail.com>
2022-10-01 21:35:05 -0700

    Add reselect library

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,10 +1,23 @@
-export const selectCategoriesMap = (state) => {
-  const categoriesMap = state.categories.categories.reduce(
-    (acc, { title, items }) => {
-      acc[title.toLowerCase()] = items;
+import { createSelector } from 'reselect'
+import Category from '../../routes/category/category.component';
+import { selectCurrentUser } from '../user/user.selector';
+
+//if input is not changes, so as output
+const selectCategoryReducer = (state) => state.categories;
+
+//create memoize selector
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories 
+)
+
+//As long as the categories array does not change, do not rerun this method
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => 
+  categories.reduce((acc, category) => {
+    const { title, items } = category;  
+    acc[title.toLowerCase()] = items;
       return acc;
-    },
-    {}
-  );
-  return categoriesMap;
-};
+    }, {})
+    )
