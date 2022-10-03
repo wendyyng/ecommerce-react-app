206d38debfa8d19d0c74d641d46c80707ccfaa00
wendyyng <ng_yih_wei@hotmail.com>
2022-10-03 10:41:41 -0700

    Add Redux-perist

--- src/index.js
+++ src/index.js
@@ -8,22 +8,19 @@ import App from './App';
 // import { CategoriesProvider } from './contexts/categories.context';
 // import { CartProvider } from './contexts/cart.context';
 import { Provider } from 'react-redux'
-import { store } from './store/store'
+import { store, persistor } from './store/store'
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
-    {/* <UserProvider> */}
-      {/* <CategoriesProvider> */}
-        {/* <CartProvider> */}
-          <App />
-        {/* </CartProvider> */}
-      {/* </CategoriesProvider> */}
-    {/* </UserProvider>/ */}
-    </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,7 @@
 import { compose, createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
 
 // root-reducer
 import { rootReducer } from './root.reducer'
@@ -19,11 +21,21 @@ const loggerMiddleware = (store) => (next) => (action) => {
     console.log('next state: ', store.getState());
 }
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
 const middleWares = [logger];
 
 //Library helpers run before an action hits the reducers
 //compose is used to pass multiple functions
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers)
+export const store = createStore(persistedReducer, undefined, composedEnhancers)
 
+export const persistor = persistStore(store);
