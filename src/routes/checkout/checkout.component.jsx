598fa68cf4c3c3b70f8f8b53ac652f1f7b0105c5
wendyyng <ng_yih_wei@hotmail.com>
2022-10-03 13:58:00 -0700

    Add serverless functions, set up Stripe payment, and add spinner button

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -4,6 +4,7 @@ import './checkout.styles.scss'
 import CheckoutItem from '../../components/checkout-item/checkout-item.component'
 import { useSelector } from 'react-redux'
 import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector'
+import PaymentForm from '../../components/payment-form/payment-form.component'
 
 const Checkout = () => {
     // const { cartItems, cartTotal } = useContext(CartContext)
@@ -46,6 +47,7 @@ const Checkout = () => {
                     )
                 })}
                 <span className="total">Total: ${cartTotal}</span>
+                <PaymentForm />
         </div>
     )
 }
---
+++ src/utils/stripe/stripe.utils.js
@@ -0,0 +1,3 @@
+import { loadStripe } from '@stripe/stripe-js'
+
+export const stripePromise = loadStripe("pk_test_51LZdCDGNV8kTfKxvU4BajbvEP5oAjRPCvkGsU8T8FscI42lCWAlRU4CBSLebbPxqJnseZ8WieEefJcNTuFmEmjv800Wa9jtN5u")
